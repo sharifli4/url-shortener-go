dff9ef161969af85500eb88c2a2302ab276ecd9a
Kenan <kenansharifov400@gmail.com>
2023-11-23 19:09:28 +0400

    refactor: handler

--- pkg/handler/url.go
+++ pkg/handler/url.go
@@ -1,11 +1,21 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/shari4ov/url-shortener-go/pkg/httpkit"
+)
 
 type HttpHandler struct {
 }
 
 func (h *HttpHandler) CreateURL(c *fiber.Ctx) error {
-	c.Write([]byte("CreateURL"))
-	return nil
+	var p httpkit.UrlPayload
+	if err := c.BodyParser(&p); err != nil {
+		return err
+	}
+	responsePayload := httpkit.ResponsePayload{
+		ShortUrl: "http://localhost:3000/123456",
+		Status:   fiber.StatusOK,
+	}
+	return c.Status(fiber.StatusOK).JSON(responsePayload)
 }
