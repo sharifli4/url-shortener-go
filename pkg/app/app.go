8db039dfd8b4b5f238b6ec4e0bc50778d15f5569
Kenan <kenansharifov400@gmail.com>
2023-11-23 18:45:04 +0400

    add: logger

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/shari4ov/url-shortener-go/pkg/handler"
 	"github.com/shari4ov/url-shortener-go/pkg/route"
 	"github.com/shari4ov/url-shortener-go/pkg/validation"
@@ -13,6 +14,7 @@ type App struct {
 
 func (a *App) Start() {
 	app := fiber.New()
+	app.Use(logger.New())
 	validate := validation.Validation{Validator: validator.New()}
 	handlers := handler.HttpHandler{}
 	routes := route.Route{
