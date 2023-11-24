64f422382bb5a41cbd9a663419f5451a718cb605
Kenan <kenansharifov400@gmail.com>
2023-11-24 18:10:05 +0400

    add: redis client

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,11 +1,14 @@
 package app
 
 import (
+	"context"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/redis/go-redis/v9"
 	"github.com/shari4ov/url-shortener-go/pkg/handler"
 	"github.com/shari4ov/url-shortener-go/pkg/route"
+	"github.com/shari4ov/url-shortener-go/pkg/service"
 	"github.com/shari4ov/url-shortener-go/pkg/validation"
 )
 
@@ -16,7 +19,11 @@ func (a *App) Start() {
 	app := fiber.New()
 	app.Use(logger.New())
 	validate := validation.Validation{Validator: validator.New()}
-	handlers := handler.HttpHandler{}
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	urlService := service.UrlService{RedisService: redisClient, Ctx: context.Background()}
+	handlers := handler.HttpHandler{Service: &urlService}
 	routes := route.Route{
 		App:           app,
 		Handlers:      &handlers,
