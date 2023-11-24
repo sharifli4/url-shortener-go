5a4ea7b41dafd62e2915a6337051f0c8300c3fdf
Kenan <kenansharifov400@gmail.com>
2023-11-24 22:17:05 +0400

    add: retry pattern for redis connection

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -10,6 +10,7 @@ import (
 	"github.com/shari4ov/url-shortener-go/pkg/route"
 	"github.com/shari4ov/url-shortener-go/pkg/service"
 	"github.com/shari4ov/url-shortener-go/pkg/validation"
+	"log"
 )
 
 type App struct {
@@ -19,9 +20,17 @@ func (a *App) Start() {
 	app := fiber.New()
 	app.Use(logger.New())
 	validate := validation.Validation{Validator: validator.New()}
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
+	var redisClient *redis.Client
+	for {
+		redisClient = redis.NewClient(&redis.Options{
+			Addr: "localhost:6379",
+		})
+		if err := redisClient.Ping(context.Background()).Err(); err != nil {
+			log.Println("connecting redis...")
+			continue
+		}
+		break
+	}
 	urlService := service.UrlService{RedisService: redisClient, Ctx: context.Background()}
 	handlers := handler.HttpHandler{Service: &urlService}
 	routes := route.Route{
