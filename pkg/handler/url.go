cb9bc177c49f3ff49c024e6da687db62f33f430a
Kenan <kenansharifov400@gmail.com>
2023-11-24 18:10:28 +0400

    add: create redirect function

--- pkg/handler/url.go
+++ pkg/handler/url.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/shari4ov/url-shortener-go/pkg/httpkit"
+	"github.com/shari4ov/url-shortener-go/pkg/service"
+	"github.com/shari4ov/url-shortener-go/pkg/token"
 )
 
 type HttpHandler struct {
+	Service *service.UrlService
 }
 
 func (h *HttpHandler) CreateURL(c *fiber.Ctx) error {
@@ -13,9 +17,27 @@ func (h *HttpHandler) CreateURL(c *fiber.Ctx) error {
 	if err := c.BodyParser(&p); err != nil {
 		return err
 	}
+
+	tokenUrl := service.UrlToken{
+		Url:   p.Url,
+		Token: token.GenerateToken(),
+	}
+	h.Service.SaveToken(tokenUrl)
 	responsePayload := httpkit.ResponsePayload{
-		ShortUrl: "http://localhost:3000/123456",
+		ShortUrl: fmt.Sprintf("http://localhost:3000/%s", tokenUrl.Token),
 		Status:   fiber.StatusOK,
 	}
 	return c.Status(fiber.StatusOK).JSON(responsePayload)
+}
+func (h *HttpHandler) RedirectURL(c *fiber.Ctx) error {
+	userToken := c.Params("token")
+	url := h.Service.GetUrl(userToken)
+	if url == "" {
+		errorPayload := httpkit.ErrorPayload{
+			Message: "Url not found",
+			Status:  fiber.StatusNotFound,
+		}
+		return c.Status(fiber.StatusNotFound).JSON(errorPayload)
+	}
+	return c.Redirect(url, fiber.StatusTemporaryRedirect)
 }
